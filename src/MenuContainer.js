b965721b1d175952ef9fd3decba556efb63a105a
LordOrbnauticus <haywood.d.johnson@gmail.com>
2018-10-10 05:14:18 -0500

    Container page work

---
+++ src/MenuContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class MenuContainer extends Component {
+    render() {
+        return(
+            <div>
+                <div>
+                    <p>This is a test to see how to do this whole sliding menu thing</p>
+                    <ul>
+                        <li>You see that little button there? Go ahead and click that!</li>
+                        <li>See that transition! Maybe we can add Greensock later to make it cooler! 😂 </li>
+                        <li>This, however is cool for now though</li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+}
