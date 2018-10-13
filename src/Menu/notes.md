00c6a3e291244855941bc47d895a109fb52881df
LordOrbnauticus <haywood.d.johnson@gmail.com>
2018-10-13 16:44:23 -0500

    Complete! Nice!

---
+++ src/Menu/notes.md
@@ -0,0 +1,5 @@
+# alright check this
+
+Bet, so recognize what is going on here. Top level is the menu. It has a vaiable attached to it that can either be `'hide'` or `'show'`. If you get show, the menu pops out thanks to the CSS that will show The menu items. Later you can use Router to create new pages. and have some fun with that <span role="img aria-label="joy"}>😂</span>
+
+We'll bind visibility as an attribute on `toggleMenu();` so it knows
--- src/MenuContainer.js
+++ src/MenuContainer.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Menu from './Menu/Menu'
+import MenuButton from './MenuButton/MenuButton';
 
 export default class MenuContainer extends Component {
     constructor(props) {
@@ -25,13 +27,17 @@ export default class MenuContainer extends Component {
 
     
     render() {
-        return(
+        return( 
             <div>
+                <MenuButton handleMouseDown={ this.handleMouseDown }/>
+                <Menu handleMouseDown={ this.handleMouseDown } 
+                menuVisibility={ this.state.visible }
+                />
                 <div>
                     <p>This is a test to see how to do this whole sliding menu thing</p>
                     <ul>
                         <li>You see that little button there? Go ahead and click that!</li>
-                        <li>See that transition! Maybe we can add Greensock later to make it cooler! 😂 </li>
+                        <li>See that transition! Maybe we can add Greensock later to make it cooler! <span role="img" aria-label="joy">😂</span> </li>
                         <li>This, however is cool for now though</li>
                     </ul>
                 </div>
